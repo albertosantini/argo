062b9ab391dcc5ecbaf0124b8371158699480799
Alberto Santini <albertosantini@gmail.com>
2015-02-11 10:59:31 +0100

    Add Gruntfile.js.

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        eslint: {
+            options: {
+                config: ".eslintrc"
+            },
+            src: [
+                "Gruntfile.js",
+                "lib/**/*.js"
+            ]
+        },
+
+        vows: {
+            all: {
+                src: "test/*.js",
+                options: {
+                    reporter: "spec",
+                    error: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-eslint");
+    grunt.loadNpmTasks("grunt-vows-runner");
+
+    grunt.registerTask("default", [
+        "eslint"
+    ]);
+};
