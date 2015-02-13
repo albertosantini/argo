9a35b336ba5de65b1078445fac364f7988314385
icebox <albertosantini@gmail.com>
2015-02-13 08:17:06 +0100

    Play with nested views.
    Get TypeError: Cannot set property '$mdGridPosition' of undefined.

--- app/js/routeConfig.js
+++ app/js/routeConfig.js
@@ -3,19 +3,26 @@
 (function () {
     angular
         .module("argo")
-        .config(config);
+        .config(config)
+        .run(setup);
 
     config.$inject = ["$stateProvider", "$urlRouterProvider"];
     function config($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state("index", {
+                abstract: true,
                 url: "/",
                 views: {
                     "default": {
                         templateUrl: "views/default.html",
                         controller: "DefaultCtrl",
                         controllerAs: "default"
-                    },
+                    }
+                }
+            })
+            .state("index.subs", {
+                url: "",
+                views: {
                     "account@index": {
                         templateUrl: "views/account.html"
                     }
@@ -24,4 +31,10 @@
 
         $urlRouterProvider.otherwise("/");
     }
+
+    setup.$inject = ["$state"];
+    function setup($state) {
+        $state.transitionTo("index.subs");
+    }
+
 }());
