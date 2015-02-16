32dbcf0c62fc190ffffbef6f8546d84990081203
Alberto Santini <albertosantini@gmail.com>
2015-02-16 11:42:19 +0100

    Add active account.

--- src/client/app/account/accounts.service.js
+++ src/client/app/account/accounts.service.js
@@ -7,23 +7,34 @@
 
     accountsService.$inject = ["$http", "environmentService"];
     function accountsService($http, environmentService) {
-        return {
-            getAccounts: function(data) {
-                var environment = data.environment,
-                    token = data.token,
-                    accountId = data.accountId,
-                    url = accountId ?
-                        "/v1/accounts" + "/" + accountId : "/v1/accounts",
-                    request = environmentService.getRequest(environment, token,
-                        "api", url);
-
-                return $http(request).then(function (response) {
-                    return response.data.accounts || response.data;
-                }, function (response) {
-                    throw response.data.message;
-                });
-            }
+        var service = {
+            activeAccount: {},
+            getAccounts: getAccounts
         };
+
+        return service;
+
+        function getAccounts(data) {
+            var environment = data.environment,
+                token = data.token,
+                accountId = data.accountId,
+                url = accountId ?
+                    "/v1/accounts" + "/" + accountId : "/v1/accounts",
+                request = environmentService.getRequest(environment, token,
+                    "api", url);
+
+            return $http(request).then(function (response) {
+                var accounts = response.data.accounts || response.data;
+
+                if (!accounts.length) {
+                    angular.extend(service.activeAccount, response.data);
+                }
+
+                return accounts;
+            }, function (response) {
+                throw response.data.message;
+            });
+        }
     }
 
 }());
