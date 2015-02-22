e0cf7dc6ca6130facfe2756c5a94c86db3027e20
icebox <albertosantini@gmail.com>
2015-02-22 12:55:36 +0100

    Remove stream endpoints from client side envs.

--- src/client/app/services/environment.service.js
+++ src/client/app/services/environment.service.js
@@ -15,16 +15,10 @@
         function getRequest(environment, type, token, url) {
             var endpoints = {
                 live: {
-                    stream: "https://stream-fxtrade.oanda.com",
                     api: "https://api-fxtrade.oanda.com"
                 },
                 practice: {
-                    stream: "http://stream-fxpractice.oanda.com",
                     api: "https://api-fxpractice.oanda.com"
-                },
-                sandbox: {
-                    stream: "http://stream-sandbox.oanda.com",
-                    api: "https://api-sandbox.oanda.com"
                 }
             };
 
