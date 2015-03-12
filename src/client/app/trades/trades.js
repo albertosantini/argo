f9ea9934ee9cfc21d9b1642452977c4f30474491
Alberto Santini <albertosantini@gmail.com>
2015-03-12 09:55:27 +0100

    Add confirmation dialog to close trade.

--- src/client/app/trades/trades.js
+++ src/client/app/trades/trades.js
@@ -5,8 +5,8 @@
         .module("argo")
         .controller("Trades", Trades);
 
-    Trades.$inject = ["toastService", "tradesService"];
-    function Trades(toastService, tradesService) {
+    Trades.$inject = ["$mdDialog", "toastService", "tradesService"];
+    function Trades($mdDialog, toastService, tradesService) {
         var vm = this;
 
         vm.getTrades = getTrades;
@@ -18,17 +18,26 @@
             vm.trades = trades;
         }
 
-        function closeTrade(id) {
-            tradesService.closeTrade(id).then(function (trade) {
-                var message = "Closed " +
-                    trade.side + " " +
-                    trade.instrument +
-                    " #" + trade.id +
-                    " @" + trade.price +
-                    " P&L " + trade.profit;
+        function closeTrade(event, id) {
+            var confirm = $mdDialog.confirm()
+                  .content("Are you sure to close the trade?")
+                  .ariaLabel("Trade closing confirmation")
+                  .ok("Ok")
+                  .cancel("Cancel")
+                  .targetEvent(event);
 
-                toastService.show(message);
-                tradesService.getTrades().then(getTrades);
+            $mdDialog.show(confirm).then(function () {
+                tradesService.closeTrade(id).then(function (trade) {
+                    var message = "Closed " +
+                        trade.side + " " +
+                        trade.instrument +
+                        " #" + trade.id +
+                        " @" + trade.price +
+                        " P&L " + trade.profit;
+
+                    toastService.show(message);
+                    tradesService.getTrades().then(getTrades);
+                });
             });
         }
     }
