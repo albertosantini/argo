6bb08c80a58b73985cc28d54be3bc845a102a4ac
Alberto Santini <albertosantini@gmail.com>
2015-03-12 09:54:42 +0100

    Add confirmation dialog to close order.

--- src/client/app/orders/orders.js
+++ src/client/app/orders/orders.js
@@ -5,8 +5,8 @@
         .module("argo")
         .controller("Orders", Orders);
 
-    Orders.$inject = ["toastService", "ordersService"];
-    function Orders(toastService, ordersService) {
+    Orders.$inject = ["$mdDialog", "toastService", "ordersService"];
+    function Orders($mdDialog, toastService, ordersService) {
         var vm = this;
 
         vm.getOrders = getOrders;
@@ -18,17 +18,26 @@
             vm.orders = orders;
         }
 
-        function closeOrder(id) {
-            ordersService.closeOrder(id).then(function (order) {
-                var message = "Closed " +
-                    order.side + " " +
-                    order.instrument +
-                    " #" + order.id +
-                    " @" + order.price +
-                    " for " + order.units;
+        function closeOrder(event, id) {
+            var confirm = $mdDialog.confirm()
+                  .content("Are you sure to close the order?")
+                  .ariaLabel("Order closing confirmation")
+                  .ok("Ok")
+                  .cancel("Cancel")
+                  .targetEvent(event);
 
-                toastService.show(message);
-                ordersService.getOrders().then(getOrders);
+            $mdDialog.show(confirm).then(function () {
+                ordersService.closeOrder(id).then(function (order) {
+                    var message = "Closed " +
+                        order.side + " " +
+                        order.instrument +
+                        " #" + order.id +
+                        " @" + order.price +
+                        " for " + order.units;
+
+                    toastService.show(message);
+                    ordersService.getOrders().then(getOrders);
+                });
             });
         }
     }
