7c7910618b095e47c558b777be1fd41423c3e6ee
Alberto Santini <albertosantini@gmail.com>
2015-03-16 09:55:47 +0100

    Fix trades service file (wrong previous commit).

--- src/client/app/trades/trades.service.js
+++ src/client/app/trades/trades.service.js
@@ -3,43 +3,84 @@
 (function () {
     angular
         .module("argo")
-        .factory("streamService", streamService);
+        .factory("tradesService", tradesService);
 
-    streamService.$inject = ["ngSocket", "quotesService",
-                        "activityService", "tradesService"];
-    function streamService(ngSocket, quotesService,
-                        activityService, tradesService) {
-        var service = {
-            getStream: getStream
-        };
+    tradesService.$inject = ["$http", "$q", "sessionService"];
+    function tradesService($http, $q, sessionService) {
+        var trades = [],
+            service = {
+                getTrades: getTrades,
+                closeTrade: closeTrade,
+                updateTrades: updateTrades,
+                refresh: refresh
+            };
 
         return service;
 
-        function getStream() {
-            var ws = ngSocket("ws://localhost:8000/stream");
+        function getTrades() {
+            return trades;
+        }
 
-            ws.onMessage(function (event) {
-                var data,
-                    tick,
-                    transaction;
+        function refresh() {
+            sessionService.isLogged().then(function (credentials) {
+                $http.post("/api/trades", {
+                    environment: credentials.environment,
+                    token: credentials.token,
+                    accountId: credentials.accountId
+                }).then(function (res) {
+                    trades.length = 0;
+                    angular.extend(trades, res.data);
+                });
+            });
+        }
 
-                try {
-                    data = angular.fromJson(event.data);
-                    tick = data.tick;
-                    transaction = data.transaction;
-                    if (tick) {
-                        quotesService.updateTick(tick);
-                        tradesService.updateTrades(tick);
+        function closeTrade(id) {
+            var deferred = $q.defer();
+
+            sessionService.isLogged().then(function (credentials) {
+                $http.post("/api/closetrade", {
+                    environment: credentials.environment,
+                    token: credentials.token,
+                    accountId: credentials.accountId,
+                    id: id
+                }).then(function (order) {
+                    deferred.resolve(order.data);
+                });
+            });
+
+            return deferred.promise;
+        }
+
+        function updateTrades(tick) {
+            trades.forEach(function (trade, index) {
+                var current;
+
+                if (trade.instrument === tick.instrument) {
+
+                    if (trade.side === "buy") {
+                        current = tick.bid;
+                        trades[index].profitPips =
+                            ((current - trade.price) / getPips(trade.price));
                     }
-                    if (transaction) {
-                        activityService.addActivity(transaction);
-                        tradesService.refresh();
+                    if (trade.side === "sell") {
+                        current = tick.ask;
+                        trades[index].profitPips =
+                            ((trade.price - current) / getPips(trade.price));
                     }
-                } catch (e) {
-                    // Discard "incomplete" json
+
+                    trades[index].current = current;
                 }
             });
         }
+
+        function getPips(n) {
+            var decimals = n.toString().split("."),
+                nDecimals = decimals[1].length,
+                pips = 1 / Math.pow(10, nDecimals - 1);
+
+            return pips;
+        }
+
     }
 
 }());
