1e956fdf6e741fcac9a80e40421f14a22618aef9
icebox <albertosantini@gmail.com>
2015-04-05 09:50:08 +0200

    Improve session service spec.

--- src/client/test/specs/session.service.spec.js
+++ src/client/test/specs/session.service.spec.js
@@ -2,18 +2,40 @@
 
 describe("sessionService", function () {
     var scope,
+        $httpBackend,
         sessionService,
         environment = "my environment",
         token = "my token",
         accountId = "my account id";
 
-    beforeEach(module("argo"));
-    beforeEach(inject(function (_$rootScope_, _sessionService_) {
-        scope = _$rootScope_.$new();
-        sessionService = _sessionService_;
-    }));
+    it("setup", function (done) {
+        module("argo");
+        inject(function (_$rootScope_, _$httpBackend_, _sessionService_) {
+            var when;
 
-    it("isLogged with setCredentials", function (done) {
+            scope = _$rootScope_.$new();
+            $httpBackend = _$httpBackend_;
+            sessionService = _sessionService_;
+
+            when = $httpBackend.whenGET;
+            when("app/layout/default.html").respond("dummy");
+            when("app/header/header.html").respond("dummy");
+            when("app/trades/trades.html").respond("dummy");
+            when("app/orders/orders.html").respond("dummy");
+            when("app/positions/positions.html").respond("dummy");
+            when("app/exposure/exposure.html").respond("dummy");
+            when("app/activity/activity.html").respond("dummy");
+            when("app/news/news.html").respond("dummy");
+            when("app/account/account.html").respond("dummy");
+            when("app/quotes/quotes.html").respond("dummy");
+            when("app/charts/charts.html").respond("dummy");
+            $httpBackend.flush();
+
+            done();
+        });
+    });
+
+    it("isLogged and setCredentials", function (done) {
         sessionService.isLogged().then(function (credentials) {
             assert(environment, credentials.environment);
             assert(token, credentials.token);
@@ -30,4 +52,15 @@ describe("sessionService", function () {
 
         scope.$apply();
     });
+
+    it("isLogged with credentials resolved", function (done) {
+        sessionService.isLogged().then(function (credentials) {
+            assert(angular.isDefined(credentials));
+
+            done();
+        });
+
+        scope.$apply();
+    });
+
 });
