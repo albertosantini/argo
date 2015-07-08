ff95bd076a5bf55da7b3f892a2ce5ab738c3076f
icebox <albertosantini@gmail.com>
2015-07-08 18:03:13 +0200

    Update angular 1.4.1, material 0.10.1-rc1.

--- karma.conf.js
+++ karma.conf.js
@@ -9,17 +9,17 @@ module.exports = function (config) {
 
         /*eslint-disable max-len */
         files: [
-            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.1/angular.min.js",
-            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.1/angular-animate.min.js",
-            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.1/angular-aria.min.js",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular-animate.min.js",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular-aria.min.js",
             "https://cdn.rawgit.com/angular-ui/ui-router/0.2.15/release/angular-ui-router.min.js",
-            "https://cdn.rawgit.com/angular/bower-material/v0.10.0/angular-material.min.js",
+            "https://cdn.rawgit.com/angular/bower-material/v0.10.1-rc1/angular-material.min.js",
             "https://cdn.rawgit.com/angular/ngSocket/master/dist/ngSocket.js",
             "https://cdn.rawgit.com/grevory/angular-local-storage/master/dist/angular-local-storage.min.js",
             "https://cdn.rawgit.com/mbostock/d3/master/d3.min.js",
             "https://cdn.rawgit.com/andredumas/techan.js/master/dist/techan.min.js",
 
-            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.1/angular-mocks.js",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular-mocks.js",
 
             "src/client/app/app.module.js",
             "src/client/app/**/*.js",
