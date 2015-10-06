e68e49cbf0c4bb93a245551ffbaa217fb9ee3b5e
icebox <albertosantini@gmail.com>
2015-10-06 19:39:20 +0200

    feat(deps): update AngularJS 1.4.7

--- karma.conf.js
+++ karma.conf.js
@@ -9,9 +9,9 @@ module.exports = function (config) {
 
         /*eslint-disable max-len */
         files: [
-            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js",
-            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular-animate.min.js",
-            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular-aria.min.js",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-animate.min.js",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-aria.min.js",
             "https://cdn.rawgit.com/angular-ui/ui-router/0.2.15/release/angular-ui-router.min.js",
             "https://cdn.rawgit.com/angular/bower-material/v0.11.2/angular-material.min.js",
             "https://cdn.rawgit.com/angular/ngSocket/master/dist/ngSocket.js",
@@ -19,7 +19,7 @@ module.exports = function (config) {
             "https://cdn.rawgit.com/mbostock/d3/master/d3.min.js",
             "https://cdn.rawgit.com/andredumas/techan.js/master/dist/techan.min.js",
 
-            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular-mocks.js",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-mocks.js",
 
             "src/client/app/app.module.js",
             "src/client/app/**/*.js",
