35774e5ddd5dd4d2f656920e8980a0bc02fc6410
icebox <albertosantini@gmail.com>
2016-01-23 13:37:26 +0100

    feat(quotes): add sparkline to quotes view
    It shows the latest 100 updates and it changes color if the trend is up or down.

--- src/client/app/components/sl.directive.js
+++ src/client/app/components/sl.directive.js
@@ -1,10 +1,6 @@
 "use strict";
 
-// From http://bl.ocks.org/vicapow/9904319
-//
-// Example
-// <argo-sl style="stroke: red; width: 2em" data="[10, 40, 20, 50, 80]"></sl>
-
+// Inspired by http://bl.ocks.org/vicapow/9904319
 (function () {
     angular
         .module("argo")
@@ -12,51 +8,75 @@
 
     argoSl.$inject = [];
     function argoSl() {
-        var directive = {
-            restrict: "E",
-            link: link,
-            replace: true,
-            template: "<svg class='sl'></svg>",
-            transclude: true
-        };
+        var data = {},
+            directive = {
+                restrict: "E",
+                link: link,
+                scope: {
+                    instrument: "=",
+                    data: "=",
+                    length: "="
+                },
+                replace: true,
+                template: "<svg class='sl'></svg>",
+                transclude: true
+            };
 
         return directive;
 
-        function link(scope, element, attrs) {
-            var svg = d3.select(element[0]);
+        function link(scope, element) {
 
-            var data = angular.fromJson(attrs.data);
-            var min = angular.isDefined(attrs.min) ? attrs.min : d3.min(data);
-            var max = angular.isDefined(attrs.max) ? attrs.max : d3.max(data);
-            var r = attrs.r || 0;
-
-            var m = r;
-            var w = svg.node().clientWidth;
-            var h = getComputedStyle(svg.node())["font-size"].replace("px", "");
-
-            var x = d3.scale.linear()
-                .domain([0, data.length - 1]).range([m, w - m]);
-            var y = d3.scale.linear()
-                .domain([min, max]).range([h - m, m]);
-
-            svg.attr({
-                width: w,
-                height: h
+            scope.$watch("data", function (quote) {
+                redraw(quote);
             });
 
-            svg.append("path").data(data)
-                .attr("d", "M" + data.map(function (d, i) {
-                    return [x(i), y(d)];
-                }).join("L"));
+            function redraw(quote) {
+                var svg = d3.select(element[0]),
+                    node = svg.node(),
+                    instrument = scope.instrument,
+                    w = node.clientWidth,
+                    h = getComputedStyle(node)["font-size"].replace("px", ""),
+                    min,
+                    max,
+                    x,
+                    y;
 
-            svg.selectAll("circle").data(data).enter().append("circle")
-                .attr("r", r)
-                .attr("cx", function (d, i) {
-                    return x(i);
-                })
-                .attr("cy", function (d) {
-                    return y(d);
+                svg.selectAll("*").remove();
+
+                if (!data[instrument]) {
+                    data[instrument] = [];
+                }
+
+                data[instrument].push(
+                    (parseFloat(quote.bid) +
+                        parseFloat(quote.ask)) / 2);
+
+                data[instrument] = data[instrument].slice(-scope.length);
+
+                if (data[instrument][0] > data[instrument].slice(-1)) {
+                    node.style.stroke = "red";
+                } else {
+                    node.style.stroke = "green";
+                }
+
+                min = d3.min(data[instrument]);
+                max = d3.max(data[instrument]);
+
+                x = d3.scale.linear()
+                    .domain([0, data[instrument].length - 1]).range([0, w]);
+                y = d3.scale.linear()
+                    .domain([min, max]).range([h, 0]);
+
+                svg.attr({
+                    width: w,
+                    height: h
                 });
+
+                svg.append("path").data(data[instrument])
+                    .attr("d", "M" + data[instrument].map(function (d, i) {
+                        return [x(i), y(d)];
+                    }).join("L"));
+            }
         }
     }
 }());
