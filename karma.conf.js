ff9e995d9952054955596cec98afb6c354adcf25
icebox <albertosantini@gmail.com>
2016-08-31 11:29:10 +0200

    chore(karma): remove local storage dep

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,6 @@ module.exports = function (config) {
             "node_modules/angular-animate/angular-animate.min.js",
             "node_modules/angular-aria/angular-aria.min.js",
             "node_modules/angular-material/angular-material.min.js",
-            "node_modules/angular-local-storage/dist/angular-local-storage.min.js",
             "node_modules/d3/build/d3.min.js",
 
             "node_modules/angular-mocks/angular-mocks.js",
