ca68c4e412b2f548ec453737d105db91d6bf379c
Alberto Santini <albertosantini@gmail.com>
2016-12-05 09:45:57 +0100

    fix(order-dialog): put bindings init in $onInit
    In 1,6.0-rc.1 CHANGELOG we have a breaking change.
    
    Previously, $compileProvider.preAssignBindingsEnabled was set to true by default. This means bindings were pre-assigned in component constructors. In Angular 1.5+ the place to put the initialization logic relying on bindings being present is the controller $onInit method.

--- src/client/app/components/order-dialog/order-dialog.js
+++ src/client/app/components/order-dialog/order-dialog.js
@@ -21,38 +21,40 @@
         var account = accountsService.getAccount(),
             pips = account.pips;
 
-        vm.changeMarket = changeMarket;
-        vm.changeMeasure = changeMeasure;
+        this.$onInit = function () {
+            vm.changeMarket = changeMarket;
+            vm.changeMeasure = changeMeasure;
 
-        vm.type = "MARKET";
-        vm.side = vm.params.side;
-        vm.instruments = vm.params.instruments;
-        vm.selectedInstrument = vm.params.selectedInstrument;
-        vm.changeMarket(vm.selectedInstrument);
-        vm.expires = [
-            {label: "1 Hour", value: 60 * 60 * 1000},
-            {label: "2 Hours", value: 2 * 60 * 60 * 1000},
-            {label: "3 Hours", value: 3 * 60 * 60 * 1000},
-            {label: "4 Hours", value: 4 * 60 * 60 * 1000},
-            {label: "5 Hours", value: 5 * 60 * 60 * 1000},
-            {label: "6 Hours", value: 6 * 60 * 60 * 1000},
-            {label: "8 Hours", value: 8 * 60 * 60 * 1000},
-            {label: "12 Hours", value: 12 * 60 * 60 * 1000},
-            {label: "18 Hours", value: 18 * 60 * 60 * 1000},
-            {label: "1 Day", value: 60 * 60 * 24 * 1000},
-            {label: "2 Days", value: 2 * 60 * 60 * 24 * 1000},
-            {label: "1 Week", value: 7 * 60 * 60 * 24 * 1000},
-            {label: "1 Month", value: 30 * 60 * 60 * 24 * 1000},
-            {label: "2 Months", value: 60 * 60 * 60 * 24 * 1000},
-            {label: "3 Months", value: 90 * 60 * 60 * 24 * 1000}
-        ];
-        vm.selectedExpire = 604800000; // 1 week
-        vm.measure = "price";
-        vm.isLowerBound = false;
-        vm.isUpperBound = false;
-        vm.isTakeProfit = false;
-        vm.isStopLoss = false;
-        vm.isTrailingStop = false;
+            vm.type = "MARKET";
+            vm.side = vm.params.side;
+            vm.instruments = vm.params.instruments;
+            vm.selectedInstrument = vm.params.selectedInstrument;
+            vm.changeMarket(vm.selectedInstrument);
+            vm.expires = [
+                {label: "1 Hour", value: 60 * 60 * 1000},
+                {label: "2 Hours", value: 2 * 60 * 60 * 1000},
+                {label: "3 Hours", value: 3 * 60 * 60 * 1000},
+                {label: "4 Hours", value: 4 * 60 * 60 * 1000},
+                {label: "5 Hours", value: 5 * 60 * 60 * 1000},
+                {label: "6 Hours", value: 6 * 60 * 60 * 1000},
+                {label: "8 Hours", value: 8 * 60 * 60 * 1000},
+                {label: "12 Hours", value: 12 * 60 * 60 * 1000},
+                {label: "18 Hours", value: 18 * 60 * 60 * 1000},
+                {label: "1 Day", value: 60 * 60 * 24 * 1000},
+                {label: "2 Days", value: 2 * 60 * 60 * 24 * 1000},
+                {label: "1 Week", value: 7 * 60 * 60 * 24 * 1000},
+                {label: "1 Month", value: 30 * 60 * 60 * 24 * 1000},
+                {label: "2 Months", value: 60 * 60 * 60 * 24 * 1000},
+                {label: "3 Months", value: 90 * 60 * 60 * 24 * 1000}
+            ];
+            vm.selectedExpire = 604800000; // 1 week
+            vm.measure = "price";
+            vm.isLowerBound = false;
+            vm.isUpperBound = false;
+            vm.isTakeProfit = false;
+            vm.isStopLoss = false;
+            vm.isTrailingStop = false;
+        };
 
         function changeMarket(instrument) {
             var price = quotesService.getQuotes()[instrument],
