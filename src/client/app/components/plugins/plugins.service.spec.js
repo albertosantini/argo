ea6d9d21991ebaa3ea2c8870f04eceeccf4a03a8
icebox <albertosantini@gmail.com>
2017-02-22 18:48:36 +0100

    test(plugins): add PluginsService test

---
+++ src/client/app/components/plugins/plugins.service.spec.js
@@ -0,0 +1,23 @@
+describe("pluginsService", () => {
+    let pluginsService;
+
+    beforeEach(module("components"));
+
+    beforeEach(inject($injector => {
+        pluginsService = $injector.get("PluginsService");
+    }));
+
+    it("getPlugins", () => {
+        const plugins = pluginsService.getPlugins();
+
+        assert.equal(true, angular.isObject(plugins));
+    });
+
+    it("getPluginsInfo", () => {
+        const pluginsInfo = pluginsService.getPluginsInfo();
+
+        assert.equal(true, angular.isObject(pluginsInfo));
+        assert.equal(0, pluginsInfo.count);
+    });
+
+});
