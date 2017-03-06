20f5fa79413c1f0846c00e3635994eb45b321485
icebox <albertosantini@gmail.com>
2017-03-06 20:25:42 +0100

    refactor(common): use tachyons in the main content
    This refactor helps to reduce material surface.

--- src/client/app/common/app.controller.js
+++ src/client/app/common/app.controller.js
@@ -2,13 +2,5 @@ export class AppController {
     $onInit() {
         this.tabSelectedIndex = 0;
     }
-
-    next() {
-        this.tabSelectedIndex = Math.min(this.tabSelectedIndex + 1, 6);
-    }
-
-    previous() {
-        this.tabSelectedIndex = Math.max(this.tabSelectedIndex - 1, 0);
-    }
 }
 AppController.$inject = [];
