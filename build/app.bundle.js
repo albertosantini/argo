607a19a4b04a687b60f6ce4a419f3f81297201e7
icebox <albertosantini@gmail.com>
2017-03-09 19:32:44 +0100

    chore(bundle): remove dead code from app.bundle.js

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -12,14 +12,6 @@ class AppController {
     $onInit() {
         this.tabSelectedIndex = 0;
     }
-
-    next() {
-        this.tabSelectedIndex = Math.min(this.tabSelectedIndex + 1, 6);
-    }
-
-    previous() {
-        this.tabSelectedIndex = Math.max(this.tabSelectedIndex - 1, 0);
-    }
 }
 AppController.$inject = [];
 
