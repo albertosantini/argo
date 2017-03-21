99836da73c771f8977b08ea2f3701e51c33b535f
icebox <albertosantini@gmail.com>
2017-03-21 19:46:51 +0100

    chore(build): rename index file for custom d3

--- rollup.config.d3-techan.js
+++ rollup.config.d3-techan.js
@@ -1,7 +1,7 @@
 import node from "rollup-plugin-node-resolve";
 
 export default {
-    entry: "index.d3-techan.js",
+    entry: "rollup.index.d3-techan.js",
     format: "umd",
     moduleName: "d3",
     plugins: [node()],
