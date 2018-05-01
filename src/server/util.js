58df5ab68e50ad7fa5dc03a1730e37015021030f
icebox <albertosantini@gmail.com>
2018-05-01 16:02:50 +0200

    chore(util): add custom request
    At the moment it is not used.
    It should replace `request` package: too muck kb e security concerns.

--- src/server/util.js
+++ src/server/util.js
@@ -1,7 +1,65 @@
 "use strict";
 
 exports.log = log;
+exports.request = request;
 
 function log(...args) {
     console.log(new Date(), ...args); /* eslint no-console:off */
+}
+
+const urlParse = require("url").parse;
+const https = require("https");
+const querystring = require("querystring");
+
+function request({
+    url = "",
+    method = "GET",
+    headers = {},
+    body = null,
+    qs = {}
+} = {}, callback) {
+    const reqUrl = urlParse(url);
+    const host = reqUrl.hostname;
+    const port = reqUrl.port || 443;
+
+    let path = reqUrl.path;
+
+    if (method === "GET") {
+        path += `?${querystring.stringify(qs)}`;
+    }
+
+    if (body) {
+        headers["Content-Type"] = "application/json";
+        headers["Content-Length"] = body.length;
+    }
+
+    const requestOptions = {
+        host,
+        port,
+        path,
+        method,
+        headers
+    };
+
+    function requestResponse(res) {
+        res.setEncoding("utf8");
+
+        let rawData = "";
+
+        res.on("data", chunk => {
+            rawData += chunk;
+        });
+
+        res.on("end", () => callback(null, res, rawData));
+    }
+
+    const req = https.request(requestOptions, requestResponse);
+
+    req.on("error", err => callback(err));
+
+    if (body) {
+        req.write(JSON.stringify(body));
+    }
+
+    req.end();
 }
