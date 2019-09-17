05b2c2362a71d58aeb660c279681526feb466a8c
icebox <albertosantini@gmail.com>
2019-09-17 20:19:04 +0200

    fix(server): fix ARGO_IP use

--- src/server/util.js
+++ src/server/util.js
@@ -93,7 +93,8 @@ function request({
 const lookup = util.promisify(dns.lookup);
 
 async function getIP() {
-    const { address } = { address: process.env.ARGO_IP } || await lookup(os.hostname());
+    const { address } = process.env.ARGO_IP
+        ? { address: process.env.ARGO_IP } : await lookup(os.hostname());
 
     return address;
 }
